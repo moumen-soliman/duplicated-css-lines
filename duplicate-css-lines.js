214c32d82f39dbf34f8054cee584dd7f0be35689
Mo'men Soliman <moumensoliman@gmail.com>
2018-12-17 22:57:29 +0200

    Update splitter to be result more spesific

--- duplicate-css-lines.js
+++ duplicate-css-lines.js
@@ -7,5 +7,10 @@ fs.readFile(process.argv[2], (err, data) =>  {
     convertedFileToObject = data.toString().split('\n');
     spaceRemover = convertedFileToObject.map(post => post.replace('{', '').replace('}', '').replace('.', '').replace(';', '').replace('img', '').replace('span', '').replace('from', '').replace(/\s+/g, '')).filter(i => i);
     findDuplicates = (arr) => arr.filter((item, index) => arr.indexOf(item) != index);
-    console.log(findDuplicates(spaceRemover).length > 0 ? findDuplicates(spaceRemover) : 'Congrats you don\'t have any duplicate')
+    const dupes = findDuplicates(spaceRemover);
+    const result = dupes.map((item) => {
+        splitted = item.split(':')
+        return { [splitted[0]]: splitted[1] };
+    });
+    console.log(result.length > 0 ? result : 'Congrats you don\'t have any duplicate')
 });
